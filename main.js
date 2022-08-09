6cb90c88ec93f475006c327e5bc135cc343bf64a
abdallamfathy <47195962+abdallamfathy@users.noreply.github.com>
2022-08-09 16:46:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -10,6 +10,83 @@ function asd(e){
     document.getElementById('OzOutput').innerHTML=ounce;
 }
 
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
+// lbsInput = document.getElementById('lbsInput');
+// lbsInput.addEventListener('input',asd);
+// function asd(e){
+//     let lbs = e.target.value;
+//     let gram = lbs/0.0022046;
+//     let kg = lbs/2;
+//     let ounce = lbs*16;
+//     document.getElementById('KilogramsOutput').innerHTML=kg;
+//     document.getElementById('gramsOutput').innerHTML=gram;
+//     document.getElementById('OzOutput').innerHTML=ounce;
+// }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                                                                                                              //
